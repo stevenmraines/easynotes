f3f0663afe0ef9c71d7a13c4f407755dd338615d
Steven M Raines <stevenmraines@gmail.com>
2019-10-11 14:47:53 -0400

    Add text to HelpTemplate.

--- src/easynotes/templates/HelpTemplate.java
+++ src/easynotes/templates/HelpTemplate.java
@@ -1,9 +1,14 @@
 package easynotes.templates;
 
+import java.awt.Font;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
 public class HelpTemplate
 {
@@ -11,7 +16,8 @@ public class HelpTemplate
 	private JDialog dialog;
 	private JFrame frame;
 	private JPanel panel;
-	private JLabel label;
+	private JLabel titleLabel;
+	private JLabel bodyLabel;
 	
 	public HelpTemplate()
 	{
@@ -20,13 +26,47 @@ public class HelpTemplate
 		frame = new JFrame("Help");
 		dialog = new JDialog(frame, true);
 		panel = new JPanel();
-		label = new JLabel("Help");
+		titleLabel = new JLabel("Basic Usage");
+		
+		String bodyText =
+			"<html>Right click on the application background to show the program context menu.<br />"
+			+ "<br />Right click on a card to show a context menu with options to edit, delete, etc.<br />"
+			+ "<br />CTRL + Left Click a card to flip it.<br />"
+			+ "<br />Double click a card to edit it.</html>";
+		
+		bodyLabel = new JLabel(bodyText);
+		
+		// Set panel layout manager
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		
+		// Give panel contents some space
+		panel.setBorder(new EmptyBorder(10, 10, 10, 10));
 		
 		// Add components
 		frame.add(panel);
-		panel.add(label);
+		panel.add(titleLabel);
+		panel.add(Box.createVerticalStrut(15));
+		panel.add(bodyLabel);
+		
+		// Enlarge JLabel font sizes
+		titleLabel.setFont(
+			new Font(
+				titleLabel.getFont().getName(),
+				titleLabel.getFont().getStyle(),
+				titleLabel.getFont().getSize() + 8
+			)
+		);
+		
+		bodyLabel.setFont(
+			new Font(
+				bodyLabel.getFont().getName(),
+				bodyLabel.getFont().getStyle(),
+				bodyLabel.getFont().getSize() + 3
+			)
+		);
 		
 		// Prepare for display
+		// TODO figure out how to center label text
 		frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		frame.pack();
 		frame.setVisible(false);
